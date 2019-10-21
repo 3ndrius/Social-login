b67e00f040a400d46df4b4a8b66f5992ff35364a
3ndrius <andrzejo1991@gmail.com>
2019-10-21 11:40:27 +0200

    Setup social login and and usage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react'
 
+import firebase from 'firebase';
+import StyledFirebaseAuth  from 'react-firebaseui/StyledFirebaseAuth'
+firebase.initializeApp({
+  // apiKey:
+  // authDomain: 
+})
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    isSignIn: false,
+  }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user=> {
+      this.setState({isSignIn:!!user})
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignIn ? 
+       <React.Fragment>
+          <p>Signed In</p> 
+        <button onClick={()=> firebase.auth().signOut()}>SignOut</button> 
+        <h1>Welcome {firebase.auth().currentUser.displayName} </h1>
+       </React.Fragment>
+        :
+       <StyledFirebaseAuth
+          uiConfig={this.uiConfig}
+          firebaseAuth={firebase.auth()}
+       />
+      }
+      </div>
+    )
+  }
 }
-
-export default App;
