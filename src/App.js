7322ce19152595a9459d5cb385ae0851ea3e76bd
3ndrius <andrzejo1991@gmail.com>
2019-11-04 17:59:26 +0100

    Clean up and route  improvements v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-
-import React, { Component } from 'react'
-import StyledFirebaseAuth  from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase';
-import { BrowserRouter, Route, Redirect,  Switch, Link} from 'react-router-dom';
-import AuthContext from './contexts/AuthContext';
-import Dashboard from './components/Dashboard';
-import Home from './components/Home';
-import Data from './components/Data'
-import Welcome from './components/Welcome';
-import Contact from './components/Contact';
+import React, { Component } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import AuthContext from "./contexts/AuthContext";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
 
 firebase.initializeApp({
-
-})
+ 
+});
 
 class AuthContextProvider extends Component {
-
   state = {
-    isSignIn: false,
-  }
+    isSignIn: false
+  };
   uiConfig = {
     signInFlow: "popup",
     signInOptions: [
@@ -29,39 +24,53 @@ class AuthContextProvider extends Component {
     callbacks: {
       signInSuccessWithAuthResult: () => false
     }
-  }
+  };
   componentDidMount = () => {
-    firebase.auth().onAuthStateChanged(user=> {
-      this.setState({isSignIn:!!user})
-    })
-  }
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignIn: !!user });
+    });
+  };
   render() {
     return (
       <BrowserRouter>
-      <AuthContext.Provider value={{isSignIn: this.state.isSignIn}}>
-      <header>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-         {this.state.isSignIn && <li><Link to='/dashboard'>Dashboard</Link></li> }
-         {this.state.isSignIn ?  <li> <button onClick={()=> firebase.auth().signOut()}>SignOut</button>  </li>
-        :
-        <StyledFirebaseAuth
-        uiConfig={this.uiConfig}
-        firebaseAuth={firebase.auth()}
-      />
-      }
-      </ul>
-      </header>
-      <Switch>
-      <Route exact path="/" component={Home} />
-      {!this.state.isSignIn &&  <Redirect path="/dashboard/" to="/"  />}
-       {this.state.isSignIn ? <Route path="/dashboard" component={Dashboard} /> : '' }
-
-      </Switch>
-      </AuthContext.Provider>
+        <AuthContext.Provider value={{ isSignIn: this.state.isSignIn }}>
+          <header>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              {this.state.isSignIn && (
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+              )}
+              {this.state.isSignIn ? (
+                <li>
+                  {" "}
+                  <button onClick={() => firebase.auth().signOut()}>
+                    SignOut
+                  </button>{" "}
+                </li>
+              ) : (
+                <StyledFirebaseAuth
+                  uiConfig={this.uiConfig}
+                  firebaseAuth={firebase.auth()}
+                />
+              )}
+            </ul>
+          </header>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {!this.state.isSignIn && <Redirect path="/dashboard/" to="/" />}
+            {this.state.isSignIn ? (
+              <Route path="/dashboard" component={Dashboard} />
+            ) : (
+              ""
+            )}
+          </Switch>
+        </AuthContext.Provider>
       </BrowserRouter>
-     
-    )
+    );
   }
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Welcome = () => {
     return (
         <div>
-            <h1>Welcome component nested</h1>
+            <h1>Welcome component nested protected</h1>
         </div>
     );
 }
