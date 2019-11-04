09dad56cab01047adb2420206d727bce0ca1c250
3ndrius <andrzejo1991@gmail.com>
2019-11-04 17:50:58 +0100

    Adding route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
-import StyledFirebaseAuth  from 'react-firebaseui/StyledFirebaseAuth'
-import firebase from 'firebase';
-import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
 
+import React, { Component } from 'react'
+import StyledFirebaseAuth  from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+import { BrowserRouter, Route, Redirect,  Switch, Link} from 'react-router-dom';
+import AuthContext from './contexts/AuthContext';
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
 import Data from './components/Data'
 import Welcome from './components/Welcome';
+import Contact from './components/Contact';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAjv_IqtZ3Ts2Cagz5wnBcwFv_du4zgUxM",
-  authDomain: "social-login-react-29e10.firebaseapp.com"
+
 })
 
-export default class App extends Component {
+class AuthContextProvider extends Component {
 
   state = {
     isSignIn: false,
@@ -22,12 +23,11 @@ export default class App extends Component {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
-      signInSuccess: () => false
+      signInSuccessWithAuthResult: () => false
     }
   }
   componentDidMount = () => {
@@ -38,33 +38,31 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <AuthContext.Provider value={{isSignIn: this.state.isSignIn}}>
       <header>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/dashboard'>Dashboard</Link></li>
-      </ul>
-      <div className="App">
-        {this.state.isSignIn ? 
-       <React.Fragment>
-          <p>Signed In</p> 
-        <button onClick={()=> firebase.auth().signOut()}>SignOut</button> 
-        <h1>Welcome </h1>
-       </React.Fragment>
+         {this.state.isSignIn && <li><Link to='/dashboard'>Dashboard</Link></li> }
+         {this.state.isSignIn ?  <li> <button onClick={()=> firebase.auth().signOut()}>SignOut</button>  </li>
         :
         <StyledFirebaseAuth
         uiConfig={this.uiConfig}
         firebaseAuth={firebase.auth()}
       />
       }
-      </div>
+      </ul>
       </header>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route path="/dashboard/welcome" component={Welcome} />
-        <Route path="/dashboard/data" component={Data} />
+      <Route exact path="/" component={Home} />
+      {!this.state.isSignIn &&  <Redirect path="/dashboard/" to="/"  />}
+       {this.state.isSignIn ? <Route path="/dashboard" component={Dashboard} /> : '' }
+
       </Switch>
+      </AuthContext.Provider>
       </BrowserRouter>
+     
     )
   }
 }
+
+export default AuthContextProvider;
